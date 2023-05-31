3f6ee211b1bf4956c3e6b1417d160e4f9d5ce02d
Sandeep Dhillon <sdhillon@linkedin.com>
2023-05-30 20:19:34 -0700

    Add more examples

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -64,3 +64,56 @@ object ExtensionMethods extends App {
   // Extension method is a pattern that's used in many languages.
 
 }
+
+object ImplicitHarmful extends App {
+
+  // Only methods that end with : are right-associative.
+  println("1" +: Seq("2", "3"))
+  // which is same as
+  println(Seq("2", "3").+:("1"))
+
+  println("1" + Seq("2", "3"))
+
+  println(Seq("2", "3") :+ "4")
+
+  // If you accidentally miss : before the +, a different implicit method
+  // would kick-in that would do string concatenation.
+  println(Seq("2", "3") + "4")
+}
+
+object ImplicitParamsDangerous extends App {
+
+  def method(a: Int)(implicit b: Int, c: Int): Int = {
+    return a + b + c
+  }
+
+  implicit val whatever: Int = 4
+
+  println(method(1)(4, 4)) // produces 9
+  println(method(1))       // produces 9
+
+}
+
+object ImplicitParamsSafe extends App {
+
+  def method(a: Int)(implicit b: First, c: Second): Int = {
+    return a + b.value + c.value
+  }
+
+  final case class First(value: Int)
+  final case class Second(value: Int)
+
+  implicit val whatever: First   = First(4)
+  implicit val whatever2: Second = Second(4)
+
+  println(method(1)(First(4), Second(4))) // produces 9
+  println(method(1))                      // produces 9
+
+  // Same function as the `implicitly` function in Predef.
+  def getIt[It](implicit it: It): It = it
+  println(getIt[First])
+
+  // Using `implicitly` directly.
+  println(implicitly[First])
+
+}
